c3d5a0100befed17a5d701c19b35ad0cdc6e48dc
plati_000 <plati_000@Cameron>
2017-09-11 11:57:01 -0400

    Finished

--- src/assignment1/A1Q10.java
+++ src/assignment1/A1Q10.java
@@ -4,6 +4,8 @@
  */
 package assignment1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author stahc1596
@@ -15,6 +17,25 @@ public class A1Q10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        //Variable will hold the letter count
+        String word = "empty";
+        //Variable will hold every word entered by user
+        System.out.println("Please enter your words. When you're done, type exit");
+        //System asks user to input their words
+        while(true){
+            word = in.nextLine();
+            //User inputs words
+            if(word.equals("exit")){
+                break;
+            }else{
+                count = count + word.length();
+            }
+            //If statement checks if the user is finished entering words
+        }
+        //While loop allows user to enter word repeatedly
+        System.out.println("The total amount of letters is " + count);
+        //System tells user the total ampunt of letters in their words
     }
 }
